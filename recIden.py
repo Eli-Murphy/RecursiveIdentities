d1a4fef0f1431fc47c5339774e9b3b2ea3d00a4b
ILikeBrunch <75328211+ILikeBrunch@users.noreply.github.com>
2021-11-08 14:12:52 -0500

    Update recIden.py

---
+++ recIden.py
@@ -0,0 +1,37 @@
+def factorial(n):
+    if n == 1:
+        return 1
+    if n > 0:
+        return n * f(n-1)
+
+def summaration(n):
+    print("Hello")
+
+def power(n):
+
+def fibonacci(n):
+
+def sond(n):
+
+def pond(n):
+
+def potwn(n):
+
+def sonir(n):
+
+def revdig(n):
+
+def euclid(n):
+
+def cib(n):
+
+def sqrtbi(n):
+
+def comboitem(n):
+
+
+
+
+def what(a,b)
+n = 10
+print(f(n))
