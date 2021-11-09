fc4c2093e8d1014df7f6015c90a1beede56de886
ILikeBrunch <75328211+ILikeBrunch@users.noreply.github.com>
2021-11-09 08:34:54 -0500

    Update recIden.py

--- recIden.py
+++ recIden.py
@@ -10,7 +10,12 @@ def summaration(n):
     if n > 0:
        return n + summaration(n-1)
 
-# def power(n):
+def power(n, p):
+    if n > 0:
+        return p * power(p,n-1)
+    if n == 1:
+        return 1
+
 
 # def fibonacci(n):
 
@@ -37,4 +42,5 @@ def summaration(n):
 
 # def what(a,b)
 n = 10
-print(summaration(n))
+p = 2
+print(power(p, n))
